49f1de723a636250b58d6abdd3c4f8180a172434
surensurendar <surender07052002@gmail.com>
2023-12-21 07:13:41 +0530

    all change crted

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 // script.js
- 
+
+let select = document.querySelectorAll('.currency')
+
+fetch('https://api.frankfurter.app/currencies')
+.then(res=>res.json())
+.then(res=>displayDropDown(res))
+
+function displayDropDown(res){
+  //console.log(Object.entries(res)[2][0])
+  let curr = Object.entries(res)
+  for(let i=0;i<curr.length;i++){
+    let opt = `<option value="${curr[i][0]}">${curr[i][0]}</option>`
+    select[0].innerHTML += opt
+    select[1].innerHTML += opt
+  }
+}
